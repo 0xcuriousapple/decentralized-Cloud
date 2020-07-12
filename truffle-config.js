da3783a47917247e0a1f4abeac825be2191fd8b8
abhishekvispute <abhivispute33@gmail.com>
2020-07-12 18:00:34 +0530

    deployed on matic

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,7 @@
 const path = require("path");
-
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -7,6 +9,13 @@ module.exports = {
   networks: {
     develop: {
       port: 8545
-    }
+    },
+    matic: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.matic.today`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
   }
 };
